ef7c6069f490d532c9314253d3802da22d4e1374
glxe <gexiang0523@163.com>
2020-06-30 10:29:15 +0800

    fix cookie

---
+++ week02/cookie_requests_demo.py
@@ -0,0 +1,41 @@
+import requests
+# from fake_useragent import UserAgent
+import time
+
+# ua = UserAgent(verify_ssl=False)
+
+header = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+    'Referer': 'https://accounts.douban.com/passport/login?source=main'
+}
+
+# 会话对象：在同一个Session实例发出的所有请求之间保持cookie
+# 期间使用urllib3的connection pooling功能
+# 向同一主机发送多个请求，底层的TCP链接将会被重用，从而带来显著的性能提升
+login_url = 'https://accounts.douban.com/j/mobile/login/basic'
+form_data = {
+    'ck': '',
+    'name': '15370383786',
+    'password': 'funcid79325',
+    'remember': 'false',
+    'ticket': ''
+}
+
+
+with requests.Session() as s:
+    # post数据前获取cookie
+    pre_login = 'https://accounts.douban.com/passport/login'
+    pre_resp = s.get(pre_login, headers=header)
+    res = s.post(url=login_url, data=form_data, headers=header, cookies=s.cookies)
+    print(res.json())
+
+
+print(res.json())
+
+order_rul = "https://www.douban.com/mine/orders/"
+res = s.get(order_rul, headers=header, cookies=s.cookies)
+
+print(res.text)
+
+
+
--- week02/method_demo.py
+++ week02/method_demo.py
@@ -1,4 +1,4 @@
-# import requests
+import requests
 
 # #GET 请求
 
@@ -14,7 +14,7 @@
 # print(r.json())
 
 
-# # 在同一个Session实例发出的所有请求之间保持cookie
+# 在同一个Session实例发出的所有请求之间保持cookie
 # s = requests.session()
 
 # s.get('https://httpbin.org/cookies/set/aaa/4444')
@@ -22,42 +22,44 @@
 # r = s.get('https://httpbin.org/cookies')
 
 # print(r.json())
-# # https://httpbin.org/cookies/set/aaa/4444
+# https://httpbin.org/cookies/set/aaa/4444
 
-# # 也可以使用上下文管理器
-# with requests.session() as s:
-#     s.get('https://httpbin.org/cookies/set/aaa/4444')
+# 也可以使用上下文管理器
+with requests.session() as s:
+    a = s.get('https://httpbin.org/cookies/set/aaa/4444')
+    print(a.json())
+
 
 
 
 
   
-from selenium import webdriver
+# from selenium import webdriver
 
-import time
+# import time
 
-try:
-    browser = webdriver.Chrome()
-    # 需要安装chrome driver, 和浏览器版本保持一致
-    # http://chromedriver.storage.googleapis.com/index.html
+# try:
+#     browser = webdriver.Chrome()
+#     # 需要安装chrome driver, 和浏览器版本保持一致
+#     # http://chromedriver.storage.googleapis.com/index.html
     
-    browser.get('https://www.douban.com')
-    time.sleep(1)
+#     browser.get('https://www.douban.com')
+#     time.sleep(1)
 
-    browser.switch_to_frame(browser.find_elements_by_tag_name('iframe')[0])
-    btm1 = browser.find_element_by_xpath('/html/body/div[1]/div[1]/ul[1]/li[2]')
-    btm1.click()
+#     browser.switch_to_frame(browser.find_elements_by_tag_name('iframe')[0])
+#     btm1 = browser.find_element_by_xpath('/html/body/div[1]/div[1]/ul[1]/li[2]')
+#     btm1.click()
 
-    browser.find_element_by_xpath('//*[@id="username"]').send_keys('15055495@qq.com')
-    browser.find_element_by_id('password').send_keys('test123test456')
-    time.sleep(1)
-    browser.find_element_by_xpath('//a[contains(@class,"btn-account")]').click()
+#     browser.find_element_by_xpath('//*[@id="username"]').send_keys('15055495@qq.com')
+#     browser.find_element_by_id('password').send_keys('test123test456')
+#     time.sleep(1)
+#     browser.find_element_by_xpath('//a[contains(@class,"btn-account")]').click()
 
-    cookies = browser.get_cookies() # 获取cookies
-    print(cookies)
-    time.sleep(3)
+#     cookies = browser.get_cookies() # 获取cookies
+#     print(cookies)
+#     time.sleep(3)
 
-except Exception as e:
-    print(e)
-finally:    
-    browser.close()
+# except Exception as e:
+#     print(e)
+# finally:    
+#     browser.close()
