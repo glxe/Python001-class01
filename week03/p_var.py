2524a7cffeb94f5339d23fd84d918b524f6964de
glxe <gexiang0523@163.com>
2020-07-14 14:23:04 +0800

    fix

--- week03/p_var.py
+++ week03/p_var.py
@@ -1,35 +1,43 @@
-# 全局变量在多个进程中不能共享，在子进程中修改全局变量对父进程中的全局变量没有影响
-# 因为父进程在创建子进程时对全局变量做了一个备份，
-# 在子进程中的全局变量与子进程的全局变量完全是不同的两个变量
-# 全局变量在多个进程中是不能被共享的。
+# # 全局变量在多个进程中不能共享，在子进程中修改全局变量对父进程中的全局变量没有影响
+# # 因为父进程在创建子进程时对全局变量做了一个备份，
+# # 在子进程中的全局变量与子进程的全局变量完全是不同的两个变量
+# # 全局变量在多个进程中是不能被共享的。
 
-import time
-import os
-from multiprocessing import Process
+# import time
+# import os
+# from multiprocessing import Process
 
-num = 1000
+# num = 1000
 
-def run():
-    print(f'我是子进程，PID：{os.getpid()}')
-    global num
-    num += 1
-    print(f'子进程num = {num}')
-    print('子进程end')
-
-
-
-if __name__ == '__main__':
-    print(f'我是父进程，PID：{os.getpid()}')
-    num += 2
-    p = Process(target=run)
-    p.start()
-    p.join()
-    # 在子进程中修改全局变量num，父进程没有任何变化
-    print(f'父进程结束，num = {num}')
+# def run():
+#     print(f'我是子进程，PID：{os.getpid()}')
+#     global num
+#     num += 1
+#     print(f'子进程num = {num}')
+#     print('子进程end')
 
 
-# 我是父进程，PID：78309
-# 我是子进程，PID：78310
-# 子进程num = 1001
-# 子进程end
-# 父进程结束，num = 1000
+
+# if __name__ == '__main__':
+#     print(f'我是父进程，PID：{os.getpid()}')
+#     num += 2
+#     p = Process(target=run)
+#     p.start()
+#     p.join()
+#     # 在子进程中修改全局变量num，父进程没有任何变化
+#     print(f'父进程结束，num = {num}')
+
+
+# # 我是父进程，PID：78309
+# # 我是子进程，PID：78310
+# # 子进程num = 1001
+# # 子进程end
+# # 父进程结束，num = 1000
+
+
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(4)
+    r = s.connect(('80.101.49.5', 899))
+    print(r)
--- week03/pmap/pmap.py
+++ week03/pmap/pmap.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import os, sys, getopt, socket
+import os, sys, getopt, socket, queue
 import re
 import struct
 import ping
+import threading
+
 
 
 
@@ -12,7 +14,7 @@ def params(argv):
     ip = '' # ip地址
     port = 80 # 端口号
     way = '' # 扫描方式
-    num = 0 # 并发数量
+    num = 1 # 并发数量
     result = 'result.json' # 扫描结果的保存文件
     v = '' # 运行时间
     m = 'thread' # 使用多线程方式扫描，或者多进程方式扫描
@@ -42,12 +44,6 @@ def params(argv):
     return ip, port, way, num, result, v, m
 
 
-def check_ip(host):
-    print(f'ip: {host}')
-    result, _ = ping.ping(host)
-    return result
-
-
 def scan_ip(ip, port):
     try:
         print(f'ip: {ip}:{port}')
@@ -67,7 +63,85 @@ def ip2int(ip):
 def int2ip(i):
     return socket.inet_ntoa(struct.pack("!I",i))
 
+
+
+
+
+
+class CheckIpThread(threading.Thread):
+    def __init__(self, start_ip, end_ip, queue):
+        super().__init__()
+        self.start_ip = start_ip
+        self.end_id = end_ip
+        self.queue = queue
+        self.active_total = 0
+    
+    def run(self):
+        print('check ip start...')
+        strat_ip_int = ip2int(self.start_ip)
+        end_ip_int = ip2int(self.end_id)
+        if (end_ip_int < strat_ip_int):
+            raise Exception('Invalid IP range! end need gt start')
+        ip_range_max = end_ip_int - strat_ip_int
+        for i in range(0, ip_range_max + 1 ):
+            host = int2ip(strat_ip_int + i)
+            print(f'ip: {host}')
+            if self.check_ip(host):
+                if not self.queue.full():
+                    self.queue.put(host)
+                    self.active_total += 1
+                    print(f'ip: {host} is active')
+                else:
+                    pass
+
+        print('check ip end.')
+        
+    def check_ip(self, host):
+        result, _ = ping.ping(host, 1)
+        return result
+
+
+class ScanIpThread(threading.Thread):
+    def __init__(self, queue, num):
+        super().__init__()
+        self.queue = queue
+        self.num = num
+
+    def run(self):
+        print(f'scan thread {self.num} ip start...')
+        while True:
+            try:
+                if not self.queue.empty():
+                    data = self.queue.get()
+                    if data == 'check_is_done':
+                        q.put('check_is_done') # 告诉其他线程，检查ip线程已经结束
+                        break
+                    print(f'scan active ip from queue: {data}, by thread {self.num}')
+                    self.scan_port(data)
+                    self.queue.task_done()
+            except Exception as e:
+                print(e)
+                pass
+        print(f'scan ip end. thread {num}')
+
+    def scan_port(self, ip):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(4)
+                for port in range(1, 1025):
+                    result_code = s.connect_ex((ip, port)) #开放放回0
+                    print(f'ip: {ip}, port: {port}, result code: {result_code}. by thread {self.num}')
+                    if (result_code == 0):
+                        print(f'is openning. by thread {self.num}')
+        except Exception as e:
+            print(e)
+        
 if __name__ == "__main__":
+
+    hostname=socket.gethostname()
+    # 获取本机IP
+    ip=socket.gethostbyname(hostname)
+    print(f'hostname: {hostname}, ip: {ip}')
     '''
     ip: 192.168.0.1 [-192.168.0.100]
     prot: 80 [1-1024]
@@ -82,20 +156,45 @@ if __name__ == "__main__":
     if not re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)-(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip):
         print('Invalid IP address!')
         sys.exit()
-    strat_ip_int = ip2int(ip.split('-')[0])
-    end_ip_int = ip2int(ip.split('-')[1])
-    if (end_ip_int < strat_ip_int):
-        print('Invalid IP range!')
-        sys.exit()
-    ip_range_max = end_ip_int - strat_ip_int
-    active_total = 0
-    for i in range(0, ip_range_max + 1 ):
-        host = int2ip(strat_ip_int + i)
-        if check_ip(host):
-            active_total += 1
-            print(f'ip: {host} is active')
+    # strat_ip_int = ip2int(ip.split('-')[0])
+    # end_ip_int = ip2int(ip.split('-')[1])
+    # if (end_ip_int < strat_ip_int):
+    #     print('Invalid IP range!')
+    #     sys.exit()
+    # ip_range_max = end_ip_int - strat_ip_int
 
-    print(f'active ip total: {active_total}')
 
+  # 任务队列，存放网页的队列
+    q = queue.Queue(200)
+    check = CheckIpThread(ip.split('-')[0], ip.split('-')[1], q)
+    check.start()
+    num = int(num)
+    scan_threads = []
+    # 将并发数量限制在 1和逻辑cpu数量一致
+    max_threads = os.cpu_count()
+    print(max_threads)
+    if int(num) > int(max_threads):
+        num = int(max_threads)
+    elif int(num) < 1:
+        num = 1
+    for n in range(1, num + 1):
+        scan = ScanIpThread(q, n)
+        scan.start()
+        scan_threads.append(scan)
+        # pass
+    
+    check.join()
+    q.put('check_is_done') # 告诉其他线程，检查ip线程已经结束
+    # while True:
+    #     if (q.empty()):
+    #         break;
+
+    #     data = q.get()
+    #     print(f'from queue ip: {data}')
+    #     q.task_done()
+
+
+    for scan in scan_threads:
+        scan.join()
     print(f"ip: {ip}, port: {port}, way: {way}, num: {num}, result: {result}, v: {v}, m: {m}")
 
